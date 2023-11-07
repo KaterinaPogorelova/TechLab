08b6c95f0f734b742e748cd510afcb618c85d23e
KaterinaPogorelova <nurichannumb73@gmail.com>
2023-11-07 14:17:28 +0100

    feat: add new constants

--- src/styledConstants.ts
+++ src/styledConstants.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
-export const colors = {
+export const StyledContainer = styled.div`
+  max-width: 1224px;
+  margin: 0 auto;
 
-};
+  @media(max-width:1440px){
+    max-width:1000px;
+    padding:0 20px;
+  }
+`;
